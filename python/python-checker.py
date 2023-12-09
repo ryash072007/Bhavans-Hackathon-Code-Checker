1bc4b6c1f1e04f58c65285517e7fbae43d40083c
Yash Raj <ryash072007@gmail.com>
2023-12-09 04:48:55 +0400

    Main Part Complete - Python Checker

---
+++ python/python-checker.py
@@ -0,0 +1,47 @@
+import subprocess
+
+correct_outputs: dict = {
+    1: "Hello World!\n",
+    2: "4152\n",
+    3: "Confirming Output (Intentionally wrong)\n"
+}
+
+class PythonChecker:
+    
+    def __init__(self, debug: bool = False):
+      self._debug = debug
+    
+    def run_python_file(self, file_path: str):
+        try:
+            output = subprocess.check_output(['python', file_path], stderr=subprocess.STDOUT, universal_newlines=True)
+            if self._debug: print(f"Output generated from {file_path} is:\n\n", output, sep = "")
+            return output
+        except subprocess.CalledProcessError as e:
+            if self._debug: print(f"Error generated from {file_path} is:\n\n", e.output, sep = "")
+            return e.output
+    
+
+    def is_correct_output(self, team_code, program_code: str, question_index: int):
+        file_path = f'team_codes/{team_code}-{question_index}.py'
+        if self._debug:
+            print("--------------- Team code:", team_code, question_index, "---------------")
+            print("Trying to write to file: ", file_path)
+        with open(file_path, 'w') as f:
+            f.write(program_code)
+        try:
+            output = self.run_python_file(file_path)
+            return output == self.get_correct_output(question_index)
+        except Exception as e:
+            if self._debug: print(f"Error occured while running {file_path}:\n", e)
+            return False
+
+    def get_correct_output(self, question_index: int):
+        return correct_outputs[question_index]
+
+# For testing purposes
+if __name__ == "__main__":
+    TEAM_CODE_MOCK = "AAAAAA"
+    PyChecker = PythonChecker(True)
+    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 'print("Hello World!")', 1))
+    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 'print(str(4152))', 2))
+    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 'print("Confirming Output")', 3))
