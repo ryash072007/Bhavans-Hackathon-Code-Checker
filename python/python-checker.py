bb2f5eee5ce631ce01bfdcd3eed05f8849aa5080
Yash Raj <ryash072007@gmail.com>
2024-04-17 10:06:22 +0400

    python checker accepts args

--- python/python-checker.py
+++ python/python-checker.py
@@ -11,9 +11,9 @@ class PythonChecker:
     def __init__(self, debug: bool = False):
       self._debug = debug
     
-    def run_python_file(self, file_path: str):
+    def run_python_file(self, file_path: str, args: list = []):
         try:
-            output = subprocess.check_output(['python', file_path], stderr=subprocess.STDOUT, universal_newlines=True)
+            output = subprocess.check_output(['python', file_path] + args, stderr=subprocess.STDOUT, universal_newlines=True)
             if self._debug: print(f"Output generated from {file_path} is:\n\n", output, sep = "")
             return output
         except subprocess.CalledProcessError as e:
@@ -21,7 +21,7 @@ class PythonChecker:
             return e.output
     
 
-    def is_correct_output(self, team_code, program_code: str, question_index: int):
+    def is_correct_output(self, team_code, question_index: int, program_code: str, args: list = []):
         file_path = f'team_codes/{team_code}-{question_index}.py'
         if self._debug:
             print("--------------- Team code:", team_code, question_index, "---------------")
@@ -29,7 +29,7 @@ class PythonChecker:
         with open(file_path, 'w') as f:
             f.write(program_code)
         try:
-            output = self.run_python_file(file_path)
+            output = self.run_python_file(file_path, args)
             return output == self.get_correct_output(question_index)
         except Exception as e:
             if self._debug: print(f"Error occured while running {file_path}:\n", e)
@@ -44,6 +44,6 @@ class PythonChecker:
 if __name__ == "__main__":
     TEAM_CODE_MOCK = "AAAAAA"
     PyChecker = PythonChecker(True)
-    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 'print("Hello World!")', 1))
-    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 'print(str(4152))', 2))
-    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 'print("Confirming Output")', 3))
+    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 1, 'print("Hello World!")'))
+    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 2, 'print(str(4152))'))
+    print(PyChecker.is_correct_output(TEAM_CODE_MOCK, 3, 'print("Confirming Output")'))
